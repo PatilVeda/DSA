8defcc364c016b7e87c2a946376436af335df621
PatilVeda <vedantpatil00h@gmail.com>
2025-08-15 11:42:29 +0530

    simple

---
+++ Logarithimic.py
@@ -0,0 +1,11 @@
+def inToStr(i):
+    digits = '0123456789'
+    if i == 0:
+        return '0'
+    result = ''
+    while i>0:
+        result =digits[i%10]+result
+        i=i//10
+    return result    
+
+# When anything going to divide or reducing it values its most probabaly the Logarithimic
