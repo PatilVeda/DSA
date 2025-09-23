bc855579bff60056c0fd2338ebcd7d13a5245130
PatilVeda <vedantpatil00h@gmail.com>
2025-09-23 18:07:43 +0530

    simple

---
+++ Untitled-1.py
@@ -0,0 +1,49 @@
+
+# a =int(input("enter a number"))
+# b=int(input("enter another number"))
+# if a>b:
+#        print("a is greater than B")
+# else:
+#        print("b is greater")
+
+
+# sum = 0
+# for i in range(5):
+#     sum=sum+i
+# print("the sum is ", sum)
+
+# for i in range(2,22,2):
+#     print(i)
+
+
+
+# i =1
+# while(i<=5):
+#     print(i)
+#     i= i+1
+
+# a=1;
+# while a<=10:
+#     print(a);
+#     a=a+2;
+
+# for i in range(6):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+
+# n = (1,3,5,7)
+# for i in n:
+#     print(i)
+#     if i==3:
+#         break
+
+
+a= int(input("enter a number"))
+b= int(input("enter a second number"))
+c=int(input("enter a third number"))
+def avrg(a,b,c):
+    sum=a+b+c
+    avg=sum/3
+    print(avg)
+avrg(a,b,c)    
